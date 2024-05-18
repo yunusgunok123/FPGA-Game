2d33b39159900307d8dba59e5f19db4986eb342b
İsmail Mert Gedik <94785857+mertgedik@users.noreply.github.com>
2024-05-18 16:05:50 +0300

    Add files via upload
    Missing file is added

---
+++ Converter/All.m
@@ -0,0 +1,4 @@
+function All(filename)
+    colorcodes = [26 28 43; 93 38 93; 178 62 83; 239 125 88; 255 205 118; 168 240 112; 54 184 101; 36 113 121; 42 54 112; 59 93 201; 65 166 246; 115 239 247; 244 244 244; 149 176 195; 86 107 134; 50 60 87];
+    PNG_converter(filename,colorcodes);
+    PNGtoVerilog("new_"+filename+".png",colorcodes);
