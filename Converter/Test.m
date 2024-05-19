270b93fb458fa088471c7fe1cb5f7ac55ab30ae2
İsmail Mert Gedik <94785857+mertgedik@users.noreply.github.com>
2024-05-19 15:26:03 +0300

    Update Test.m

--- Converter/Test.m
+++ Converter/Test.m
@@ -1,5 +1,5 @@
 % Type the filename of the png or jpg file without using the extention
 % For example, "deneme" is correct but "deneme.png" is wrong
-% Type the registername for the verilog code as a strings such as "register_1"
+% Type the registername for the verilog code as a string such as "register_1"
 
 All("filename","register")
