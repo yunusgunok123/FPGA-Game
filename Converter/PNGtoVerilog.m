f905abf9323191b3073db5805e25f25aa5ca4dd9
yunusgunok123 <91676137+yunusgunok123@users.noreply.github.com>
2024-05-25 16:30:43 +0300

    Update PNGtoVerilog.m

--- Converter/PNGtoVerilog.m
+++ Converter/PNGtoVerilog.m
@@ -1,13 +1,11 @@
-function PNGtoVerilog(filename, colorPalette, registername)
-    % Validate the color palette
-    numColors = size(colorPalette, 1);
-    if size(colorPalette, 2) ~= 3
-        error('Color palette must be an Nx3 matrix representing RGB colors.');
-    end
+colorcodes = [26 28 43; 93 38 93; 178 62 83; 239 125 88; 255 205 118; 168 240 112; 54 184 101; 36 113 121; 42 54 112; 59 93 201; 65 166 246; 115 239 247; 244 244 244; 149 176 195; 86 107 134; 50 60 87];
 
-    % Determine the number of bits needed to represent the palette indices
-    numBits = ceil(log2(numColors));
-    
+PNGtoVerilog("space_ship_0.png", colorcodes)
+
+function PNGtoVerilog(filename, colorPalette)
+
+    palette_len = length(colorPalette);
+
     % Read the image
     img = imread(filename);
     [height, width, ~] = size(img);
@@ -16,28 +14,59 @@ function PNGtoVerilog(filename, colorPalette, registername)
     [~, name, ~] = fileparts(filename);
     verilogFilename = strcat(name, '.txt');
     fid = fopen(verilogFilename, 'w');
-    
-    % Iterate through each pixel and write the Verilog code to assign values
+
+    fprintf(fid, 'function [3:0] %s(input [8:0] x, input [8:0] y);\n', name);
+    fprintf(fid, 'begin\n');
+    fprintf(fid, 'case(y)\n');
+
     for y = 1:height
+        fprintf(fid, "9'd%d: begin\n", y-1);
+        fprintf(fid, 'case(x)\n');
+        
+        row = zeros(1, width);
         for x = 1:width
-            % Get the RGB values of the pixel
             pixel = squeeze(img(y, x, :))';
-            
-            % Find the index of the matching color in the palette
-            colorIndex = find(all(colorPalette == pixel, 2)) - 1;
-            
-            if isempty(colorIndex)
-                error('Pixel color not found in the palette.');
-            end
-            
-            % Write the assignment to the file
-            fprintf(fid, '    '+registername+'[%d][%d] = %d''h%X;\n', x-1, y-1 , numBits, colorIndex);
+            row(x) = find(all(colorPalette == pixel, 2));
         end
-    end
+        
+        countOccurrences = zeros(1, palette_len);
+        indexes = cell(1, palette_len);
+        
+        for i = 1:palette_len
+            indexes{i} = find(row == i);
+            countOccurrences(i) = length(indexes{i});
+        end
     
+        [~, maxIndex] = max(countOccurrences);
+
+        for i = 1:palette_len
+            if i == maxIndex
+                fprintf(fid, "default: %s = 4'd%d;\n", name, i - 1);
+            else
+                len = countOccurrences(i);
+                if len ~= 0
+                    for j = 1:len
+                        fprintf(fid, "9'd%d", indexes{i}(j));
+                        if j ~= len
+                            fprintf(fid, ',');
+                        end
+                    end
+                    fprintf(fid, ': %s = %d;\n', name, i - 1);
+                end
+            end
+        end
+
+        fprintf(fid, 'endcase\n');
+        fprintf(fid, 'end\n');
+    end
+
+    fprintf(fid, 'endcase\n');
+    fprintf(fid, 'end\n');
+    fprintf(fid, 'endfunction\n');
+        
     % Close the file
     fclose(fid);
     
     % Display a message to indicate completion
-    fprintf('Verilog code generated and saved to %s\n', verilogFilename);
+    fprintf('Text file generated and saved to %s\n', verilogFilename);
 end
